4642e839eed0608564725cf94699e674e40d4c4a
théodore clément <213218350+thclemnt@users.noreply.github.com>
2025-12-08 16:32:40 +0100

    feat: v2 compatible user_webhook form

--- src/UserWebhook.php
+++ src/UserWebhook.php
@@ -10,7 +10,8 @@ use Plugin;
 use Session;
 use User;
 
-class UserWebhook extends CommonDBRelation {
+class UserWebhook extends CommonDBRelation
+{
     use Permissions;
 
     // Relation: User <-> Webhook
@@ -21,25 +22,44 @@ class UserWebhook extends CommonDBRelation {
 
     public static $checkItem_2_Rights = self::DONT_CHECK_ITEM_RIGHTS;
 
-    public static function getTable($classname = null) {
+    public static function getTable($classname = null)
+    {
         return 'glpi_plugin_webhook_user_webhooks';
     }
 
-    public static function getTypeName($nb = 0) {
+    public static function getTypeName($nb = 0)
+    {
         return _n('User webhook', 'User webhooks', $nb, 'webhook');
     }
 
-    public static function getFormURL($full = false) {
+    public static function getFormURL($full = false)
+    {
         return Plugin::getWebDir('webhook') . '/front/userwebhook.form.php';
     }
 
-    public static function canView() { return self::canUse(); }
-    public static function canCreate() { return self::canConfig(); }
-    public static function canUpdate() { return self::canConfig(); }
-    public static function canDelete() { return self::canConfig(); }
-    public static function canPurge() { return self::canConfig(); }
+    public static function canView()
+    {
+        return self::canUse();
+    }
+    public static function canCreate()
+    {
+        return self::canConfig();
+    }
+    public static function canUpdate()
+    {
+        return self::canConfig();
+    }
+    public static function canDelete()
+    {
+        return self::canConfig();
+    }
+    public static function canPurge()
+    {
+        return self::canConfig();
+    }
 
-    public static function install() {
+    public static function install()
+    {
         global $DB;
         $table = self::getTable();
         if (!$DB->tableExists($table)) {
@@ -62,7 +82,8 @@ SQL;
         return true;
     }
 
-    public static function uninstall() {
+    public static function uninstall()
+    {
         global $DB;
         $table = self::getTable();
         if ($DB->tableExists($table)) {
@@ -71,7 +92,8 @@ SQL;
         return true;
     }
 
-    public function getTabNameForItem(CommonGLPI $item, $withtemplate = 0) {
+    public function getTabNameForItem(CommonGLPI $item, $withtemplate = 0)
+    {
         if ($item->getType() === 'User' && self::canView()) {
             $nb = 0;
             if ($_SESSION['glpishow_count_on_tabs']) {
@@ -82,7 +104,8 @@ SQL;
         return '';
     }
 
-    public static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0) {
+    public static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0)
+    {
         if ($item->getType() === 'User') {
             self::showForUser($item);
         }
@@ -94,7 +117,8 @@ SQL;
      *
      * @param User $user
      */
-    public static function showForUser(User $user) {
+    public static function showForUser(User $user)
+    {
         global $DB;
 
         $ID = $user->getID();
@@ -105,38 +129,81 @@ SQL;
         $canedit = self::canConfig() || Session::haveRight('plugin_webhook_use', UPDATE);
         $rand = mt_rand();
 
-        // Add form
         if ($canedit) {
-            echo "<div class='firstbloc'>";
-            echo "<form name='userwebhook_form$rand' id='userwebhook_form$rand' method='post' action='" . self::getFormURL() . "'>";
-            echo "<table class='tab_cadre_fixe'>";
-            echo "<tr class='tab_bg_1'><th colspan='4'>" . __('Add a webhook to user', 'webhook') . "</th></tr>";
+            if (function_exists('renderTwigForm')) {
+                $form = [
+                    'action' => self::getFormURL(),
+                    'itemtype' => self::class,
+                    'content' => [
+                        __('Add a webhook to user', 'webhook') => [
+                            'visible' => true,
+                            'inputs' => [
+                                '' => [
+                                    'type' => 'hidden',
+                                    'name' => 'users_id',
+                                    'value' => $ID,
+                                ],
+                                __('Webhook', 'webhook') => [
+                                    'type' => 'select',
+                                    'name' => 'plugin_webhook_webhooks_id',
+                                    'itemtype' => Webhook::class,
+                                    'condition' => [
+                                        'is_active' => 1,
+                                        'entities_id' => $user->getEntityID(),
+                                    ],
+                                    'display_emptychoice' => true,
+                                ],
+                                __('Active') => [
+                                    'type' => 'checkbox',
+                                    'name' => 'is_active',
+                                    'value' => 1,
+                                    'checked' => true,
+                                ],
+                            ],
+                        ],
+                    ],
+                    'buttons' => [
+                        [
+                            'type' => 'submit',
+                            'name' => 'add',
+                            'value' => _sx('button', 'Add'),
+                            'class' => 'btn btn-secondary',
+                        ],
+                    ],
+                ];
+                renderTwigForm($form);
+            } else {
+                echo "<div class='firstbloc'>";
+                echo "<form name='userwebhook_form$rand' id='userwebhook_form$rand' method='post' action='" . self::getFormURL() . "'>";
+                echo "<table class='tab_cadre_fixe'>";
+                echo "<tr class='tab_bg_1'><th colspan='4'>" . __('Add a webhook to user', 'webhook') . "</th></tr>";
 
-            echo "<tr class='tab_bg_2'>";
-            echo "<td>" . __('Webhook', 'webhook') . "</td>";
-            echo "<td>";
-            echo Html::hidden('users_id', ['value' => $ID]);
-            Dropdown::show('PluginWebhookWebhook', [
-                'name'      => 'plugin_webhook_webhooks_id',
-                'entity'    => $user->getEntityID(),
-                'comments'  => false,
-                'condition' => ['is_active' => 1]
-            ]);
-            echo "</td>";
-            echo "<td>" . __('Active') . "</td>";
-            echo "<td>";
-            Dropdown::showYesNo('is_active', 1);
-            echo "</td>";
-            echo "</tr>";
+                echo "<tr class='tab_bg_2'>";
+                echo "<td>" . __('Webhook', 'webhook') . "</td>";
+                echo "<td>";
+                echo Html::hidden('users_id', ['value' => $ID]);
+                Dropdown::show('PluginWebhookWebhook', [
+                    'name' => 'plugin_webhook_webhooks_id',
+                    'entity' => $user->getEntityID(),
+                    'comments' => false,
+                    'condition' => ['is_active' => 1]
+                ]);
+                echo "</td>";
+                echo "<td>" . __('Active') . "</td>";
+                echo "<td>";
+                Dropdown::showYesNo('is_active', 1);
+                echo "</td>";
+                echo "</tr>";
 
-            echo "<tr class='tab_bg_2'>";
-            echo "<td colspan='4' class='center'>";
-            echo "<input type='submit' name='add' value=\"" . _sx('button', 'Add') . "\" class='submit'>";
-            echo "</td></tr>";
+                echo "<tr class='tab_bg_2'>";
+                echo "<td colspan='4' class='center'>";
+                echo "<input type='submit' name='add' value=\"" . _sx('button', 'Add') . "\" class='submit'>";
+                echo "</td></tr>";
 
-            echo "</table>";
-            Html::closeForm();
-            echo "</div>";
+                echo "</table>";
+                Html::closeForm();
+                echo "</div>";
+            }
         }
 
         // List existing webhooks for user
@@ -146,87 +213,118 @@ SQL;
                 Webhook::getTable() . '.name AS webhook_name',
                 Webhook::getTable() . '.url AS webhook_url'
             ],
-            'FROM'   => self::getTable(),
+            'FROM' => self::getTable(),
             'LEFT JOIN' => [
                 Webhook::getTable() => [
                     'FKEY' => [
-                        self::getTable()    => 'plugin_webhook_webhooks_id',
+                        self::getTable() => 'plugin_webhook_webhooks_id',
                         Webhook::getTable() => 'id'
                     ]
                 ]
             ],
-            'WHERE'  => ['users_id' => $ID],
-            'ORDER'  => 'webhook_name ASC'
+            'WHERE' => ['users_id' => $ID],
+            'ORDER' => 'webhook_name ASC'
         ]);
 
-        $num = count($iterator);
+        $rows = iterator_to_array($iterator);
+        $num = count($rows);
 
-        echo "<div class='spaced'>";
-        Html::openMassiveActionsForm('mass' . __CLASS__ . $rand);
-
-        if ($canedit && $num) {
-            $massiveactionparams = [
-                'num_displayed' => min($_SESSION['glpilist_limit'], $num),
-                'container'     => 'mass' . __CLASS__ . $rand
+        if (function_exists('renderTwigForm')) {
+            $fields = [
+                __('Webhook', 'webhook'),
+                __('URL'),
+                __('Active'),
             ];
-            Html::showMassiveActions($massiveactionparams);
-        }
 
-        echo "<table class='tab_cadre_fixehov'>";
-        $header_begin  = "<tr>";
-        $header_top    = '';
-        $header_bottom = '';
-        $header_end    = '';
-
-        if ($canedit && $num) {
-            $header_begin  .= "<th width='10'>";
-            $header_top    .= Html::getCheckAllAsCheckbox('mass' . __CLASS__ . $rand);
-            $header_bottom .= Html::getCheckAllAsCheckbox('mass' . __CLASS__ . $rand);
-            $header_end    .= "</th>";
-        }
-
-        $header_end .= "<th>" . __('Webhook', 'webhook') . "</th>";
-        $header_end .= "<th>" . __('URL') . "</th>";
-        $header_end .= "<th>" . __('Active') . "</th>";
-        $header_end .= "</tr>";
-
-        echo $header_begin . $header_top . $header_end;
-
-        if ($num > 0) {
-            foreach ($iterator as $data) {
-                echo "<tr class='tab_bg_1'>";
-
-                if ($canedit) {
-                    echo "<td width='10'>";
-                    Html::showMassiveActionCheckBox(__CLASS__, $data['id']);
-                    echo "</td>";
-                }
-
-                echo "<td>";
+            $values = [];
+            $massiveValues = [];
+            foreach ($rows as $data) {
+                $row = [];
                 $webhookUrl = Plugin::getWebDir('webhook') . '/front/webhook.form.php?id=' . $data['plugin_webhook_webhooks_id'];
-                echo "<a href='$webhookUrl'>" . Html::entities_deep($data['webhook_name'] ?? __('Unknown')) . "</a>";
-                echo "</td>";
-
-                echo "<td>" . Html::entities_deep($data['webhook_url'] ?? '') . "</td>";
-
-                echo "<td>" . Dropdown::getYesNo($data['is_active']) . "</td>";
-
-                echo "</tr>";
+                $row[] = "<a href='$webhookUrl'>" . Html::entities_deep($data['webhook_name'] ?? __('Unknown')) . "</a>";
+                $row[] = Html::entities_deep($data['webhook_url'] ?? '');
+                $row[] = Dropdown::getYesNo($data['is_active']);
+                $values[] = $row;
+                if ($canedit) {
+                    $massiveValues[] = 'item[' . __CLASS__ . '][' . $data['id'] . ']';
+                }
             }
+
+            renderTwigTemplate('table.twig', [
+                'id' => 'TableForUserWebhook' . $rand,
+                'fields' => $fields,
+                'values' => $values,
+                $canedit ? 'massive_action' : '' => $massiveValues,
+                'noToolBar' => true,
+            ]);
         } else {
-            echo "<tr class='tab_bg_1'><td colspan='4' class='center'>" . __('No webhook assigned to this user', 'webhook') . "</td></tr>";
+            // Legacy table (v1)
+            echo "<div class='spaced'>";
+            Html::openMassiveActionsForm('mass' . __CLASS__ . $rand);
+
+            if ($canedit && $num) {
+                $massiveactionparams = [
+                    'num_displayed' => min($_SESSION['glpilist_limit'], $num),
+                    'container' => 'mass' . __CLASS__ . $rand
+                ];
+                Html::showMassiveActions($massiveactionparams);
+            }
+            echo "<table class='tab_cadre_fixehov'>";
+            $header_begin = "<tr>";
+            $header_top = '';
+            $header_bottom = '';
+            $header_end = '';
+
+            if ($canedit && $num) {
+                $header_begin .= "<th width='10'>";
+                $header_top .= Html::getCheckAllAsCheckbox('mass' . __CLASS__ . $rand);
+                $header_bottom .= Html::getCheckAllAsCheckbox('mass' . __CLASS__ . $rand);
+                $header_end .= "</th>";
+            }
+
+            $header_end .= "<th>" . __('Webhook', 'webhook') . "</th>";
+            $header_end .= "<th>" . __('URL') . "</th>";
+            $header_end .= "<th>" . __('Active') . "</th>";
+            $header_end .= "</tr>";
+
+            echo $header_begin . $header_top . $header_end;
+
+            if ($num > 0) {
+                foreach ($rows as $data) {
+                    echo "<tr class='tab_bg_1'>";
+
+                    if ($canedit) {
+                        echo "<td width='10'>";
+                        Html::showMassiveActionCheckBox(__CLASS__, $data['id']);
+                        echo "</td>";
+                    }
+
+                    echo "<td>";
+                    $webhookUrl = Plugin::getWebDir('webhook') . '/front/webhook.form.php?id=' . $data['plugin_webhook_webhooks_id'];
+                    echo "<a href='$webhookUrl'>" . Html::entities_deep($data['webhook_name'] ?? __('Unknown')) . "</a>";
+                    echo "</td>";
+
+                    echo "<td>" . Html::entities_deep($data['webhook_url'] ?? '') . "</td>";
+
+                    echo "<td>" . Dropdown::getYesNo($data['is_active']) . "</td>";
+
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr class='tab_bg_1'><td colspan='4' class='center'>" . __('No webhook assigned to this user', 'webhook') . "</td></tr>";
+            }
+
+            echo $header_begin . $header_bottom . $header_end;
+            echo "</table>";
+
+            if ($canedit && $num) {
+                $massiveactionparams['ontop'] = false;
+                Html::showMassiveActions($massiveactionparams);
+            }
+
+            Html::closeForm();
+            echo "</div>";
         }
-
-        echo $header_begin . $header_bottom . $header_end;
-        echo "</table>";
-
-        if ($canedit && $num) {
-            $massiveactionparams['ontop'] = false;
-            Html::showMassiveActions($massiveactionparams);
-        }
-
-        Html::closeForm();
-        echo "</div>";
     }
 
     /**
@@ -236,7 +334,8 @@ SQL;
      * @param bool $active_only Only return active webhooks
      * @return array
      */
-    public static function getWebhooksForUser(int $users_id, bool $active_only = true): array {
+    public static function getWebhooksForUser(int $users_id, bool $active_only = true): array
+    {
         global $DB;
 
         $criteria = ['users_id' => $users_id];
@@ -246,8 +345,8 @@ SQL;
 
         $iterator = $DB->request([
             'SELECT' => ['plugin_webhook_webhooks_id'],
-            'FROM'   => self::getTable(),
-            'WHERE'  => $criteria
+            'FROM' => self::getTable(),
+            'WHERE' => $criteria
         ]);
 
         $webhooks = [];
